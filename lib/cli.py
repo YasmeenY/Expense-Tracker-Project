a293734e1f80e64304c2b6096fe9be73a29a4379
colelevy <colelevy08@gmail.com>
2023-05-15 13:16:53 -0400

    Updates

--- lib/cli.py
+++ lib/cli.py
@@ -1,87 +1,87 @@
 from expense import Expense
-import sqlite3
+from earnings import Earnings
+from compare import compare
 
+import sqlite3
 
 CONN = sqlite3.connect("database.db")
 CURSOR = CONN.cursor()
 
 while True:
-    print("Select an option:")
-    print("1. Expenses")
-    print("2. Earnings")
+    try:
+        print("Select an option:")
+        print("1. Expenses")
+        print("2. Earnings")
+        print("3. Compare Expenses and Earnings")
 
-    choice = int(input("Pick a Number:"))
+        choice = int(input("Pick a Number:"))
 
-    if choice == 1:
-        print("1. View Total Expenses in each category")
-        print("2. Add New Expense")
-        print("3. Add New Expense")
-        print("4. Remove Expense")
+        if choice == 1:
+            print("1. View Total Expenses in each category")
+            print("2. Add New Expense")
+            print("3. Remove Expense")
 
-        expense_choice = int(input("Pick a Number:"))
+            expense_choice = int(input("Pick a Number:"))
 
-        if expense_choice == 2:
-            category = input("What category is it ? Food, Clothes, Travel, Utilities, Rent, Misc. or Bills ? ")
-            Expense.find_by_category(category)
-            exit()
-        elif expense_choice == 3:
-            name = input("What did you buy: ")
-            category = input("What category was it ? Food, Clothes, Travel, Utilities, Rent, Misc. or Bills ? ")
-            price = input("How much did you spend: ")
-            Expense.create(name, category, price)
-
-            continue_choice = int(input("Pick 1 to return to main menu or press 0 to exit: "))
-
-            if (continue_choice) == 1:
-                continue
-            if (continue_choice) == 0:
-                exit()
+            if expense_choice == 1:
+                category = input("Which category do you want to see? Food, Clothes, Travel, Utilities, Rent, Misc. or Bills ? ")
+                print(Expense.find_by_category(category))
+            elif expense_choice == 2:
+                name = input("What did you buy: ")
+                category = input("What category was it ? Food, Clothes, Travel, Utilities, Rent, Misc. or Bills ? ")
+                price = float(input("How much did you spend: "))
+                Expense.create(name, category, price)
+            elif expense_choice == 3:
+                name = input("What expense do you want to remove: ")
+                category = input("What category was it ? Food, Clothes, Travel, Utilities, Rent, Misc. or Bills ? ")
+                Expense.delete(name, category)
             else:
                 print("Invalid Choice Exit")
                 exit()
-        elif earning_choice == 1:
-            print("View Total Expenses in each category")
-            exit()
-        elif earning_choice == 4:
-            print("Remove Expense")
-            exit()
-        else:
-            print("Invalid Choice Exit")
-            exit()
 
-    elif choice == 2:
-        print("1. View Total Earnings in each category")
-        print("2. View Earnings By Category")
-        print("3. Add New Earning")
-        print("4. Remove Expense")
+        elif choice == 2:
+            print("1. View Total Earnings in each category")
+            print("2. Add New Earning")
+            print("3. Remove Earning")
 
-        earning_choice = int(input("Pick a Number:"))
+            earning_choice = int(input("Pick a Number:"))
 
-        if earning_choice == 1:
-            print("View Total Earnings in each category")
-            exit()
-        elif earning_choice == 3:
-            print("Add Earning")
-            continue_choice = int(input("Pick 1 to return to main menu or press 0 to exit: "))
-            
-            if (continue_choice) == 1:
-                continue
-            if (continue_choice) == 0:
-                exit()
+            if earning_choice == 1:
+                category = input("Which category do you want to see? Passive Income, Salary, Freelance, Gifts ? ")
+                print(Earnings.find_by_category(category))
+            elif earning_choice == 2:
+                name = input("Where are the earning from?: ")
+                category = input("What category was it ? Passive Income, Salary, Freelance, Gifts ? ")
+                amount = float(input("How much did you earn: "))
+                Earnings.create(name, category, amount)
+            elif earning_choice == 3:
+                name = input("What earning do you want to remove: ")
+                category = input("What category was it ? Passive Income, Salary, Freelance, Gifts ? ")
+                Earnings.delete(name, category)
             else:
                 print("Invalid Choice Exit")
                 exit()
-        elif earning_choice == 2:
-            print("View Earnings By Category")
-            exit()
-        elif earning_choice == 4:
-            print("Remove Expense")
+
+        elif choice == 3:
+            print(compare())
 
         else:
             print("Invalid Choice Exit")
             exit()
 
-    else:
-        print("Invalid Choice Exit")
-        exit()
-        
+        # Ask the user if they want to continue or exit the program
+        continue_choice = int(input("\nWould you like to do something else? Pick 1 to return to the main menu or 0 to exit: "))
+
+        if continue_choice == 1:
+            continue
+        elif continue_choice == 0:
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Exiting the program. Goodbye!")
+            break
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
+
--- lib/earnings.py
+++ lib/earnings.py
@@ -1,7 +1,46 @@
 import sqlite3
 
-CONN = sqlite3.connect("database.db")
-CURSOR = CONN.cursor()
+class Earnings:
+    @staticmethod
+    def create(name, category, amount):
+        # Connect to the database
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
 
-class Earnings():
-    pass
+        # Insert a new earning into the 'earnings' table
+        cursor.execute("INSERT INTO earnings (name, category, amount) VALUES (?, ?, ?)", (name, category, amount))
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def find_by_category(category):
+        # Connect to the database
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        # Select all earnings with the provided category
+        cursor.execute("SELECT * FROM earnings WHERE category = ?", (category,))
+
+        # Fetch all the matching records
+        rows = cursor.fetchall()
+
+        # Close the connection
+        conn.close()
+
+        # Return the fetched records
+        return rows
+
+    @staticmethod
+    def delete(name, category):
+        # Connect to the database
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        # Delete the earning with the provided name and category
+        cursor.execute("DELETE FROM earnings WHERE name = ? AND category = ?", (name, category))
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
